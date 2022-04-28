decb8cd82d22889172f327a043ec64dba5b9ab3b
a1konstantin <90243224+a1konstantin@users.noreply.github.com>
2022-04-28 10:43:28 +0400

    28.04

--- app/src/main/java/com/bignerdranch/android/quibiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/quibiz/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -14,9 +15,15 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         trueButton.setOnClickListener { view: View ->
-            // Что-то выполнить после нажатия
+            Toast.makeText(this,
+            R.string.correct_toast,Toast.LENGTH_SHORT).show()
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            Toast.makeText(this,
+                R.string.incorrect_toast,Toast.LENGTH_SHORT).show()
+        }
         }
     }
 
 
-}
